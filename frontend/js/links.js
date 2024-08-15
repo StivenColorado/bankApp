4e8434cee95456632a647c8d08fac8a61cc79ad9
stiven colorado <stivenchoo@gmail.com>
2024-08-14 20:14:19 -0500

    building routes

---
+++ frontend/js/links.js
@@ -0,0 +1,20 @@
+// archivo para rutas, para detectar eventos de click en un boton y redireccionar
+
+window.addEventListener('DOMContentLoaded', () => {
+    // definiendo botones
+    let btn_index = document.querySelector('button[name="index_consulte_aqui"]')
+    let btn_form = document.querySelector('button[name="form_continuar"]')
+
+    presionar(btn_index, 'pages/form')
+    presionar(btn_form,'services')
+})
+
+
+
+const presionar = (boton, url) => {
+  if(boton){
+    boton.addEventListener('click', () => {
+        window.location.href = `${url}.html`
+    })
+  }
+}
